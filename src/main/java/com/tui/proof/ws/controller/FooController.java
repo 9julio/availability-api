021fd6bb70539ddd56810b6ef859caf357d734aa
9julio <jafernandezg9@gmail.com>
2021-04-09 15:44:10 +0200

    Initial commit

--- src/main/java/com/tui/proof/ws/controller/FooController.java
+++ src/main/java/com/tui/proof/ws/controller/FooController.java
@@ -9,7 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class FooController {
 
   @PostMapping("/")
-  void test() {
+  public void test() {
     log.info("Foo controller");
   }
+
 }
